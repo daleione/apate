ebfa7aa4f65f42c158bf25d87ade249e403a73b8
dalei <guoyunlei@live.com>
2018-05-28 13:50:04 +0800

    Support autoreloading

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -1,12 +1,25 @@
 extern crate actix_web;
+extern crate listenfd;
+
 extern crate apate;
 
 use actix_web::{server, App};
+use listenfd::ListenFd;
+
 use apate::index;
 
 fn main() {
-    server::new( || App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run();
+    let mut listenfd = ListenFd::from_env();
+    let mut server = server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(index))
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)
+    } else {
+        server.bind("127.0.0.1:8088").unwrap()
+    };
+
+    server.run();
 }
