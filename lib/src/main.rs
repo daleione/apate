12bd76d903ae326c4342a63a9fd3d9b0286887a7
dalei <guoyunlei@live.com>
2018-07-26 11:40:04 +0800

    Update server bind address

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
     } else {
-        server.bind("127.0.0.1:8088").unwrap()
+        server.bind("0.0.0.0:8088").unwrap()
     };
 
     server.run();
