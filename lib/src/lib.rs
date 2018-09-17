53e1bedda3a50ba7c5999d699af90a61519c475d
dalei <guoyunlei@live.com>
2018-09-17 12:15:42 +0800

    Update depency and add ping

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,22 @@
 extern crate actix_web;
 
 use actix_web::{HttpRequest, Responder};
+use std::cell::Cell;
 
 
-pub fn index(_req: &HttpRequest) -> impl Responder {
-    "Hello world!"
+#[derive(Debug, Default)]
+pub struct AppState {
+    pub counter: Cell<usize>,
+}
+
+
+pub fn index(request: &HttpRequest<AppState>) -> impl Responder {
+    println!("counter is {}", request.state().counter.get());
+    let count = request.state().counter.get() + 1;
+    request.state().counter.set(count);
+    format!("Request number is {}", count)
+}
+
+pub fn ping(_request: &HttpRequest) -> impl Responder {
+    format!("pong")
 }
--- lib/src/main.rs
+++ lib/src/main.rs
@@ -3,17 +3,28 @@ extern crate listenfd;
 
 extern crate apate;
 
-use actix_web::{server, App};
+use actix_web::{server, App, HttpResponse};
 use listenfd::ListenFd;
 
-use apate::index;
+use apate::{AppState, index, ping};
 
 fn main() {
     let mut listenfd = ListenFd::from_env();
     let mut server = server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(index))
-    });
+        vec![
+            App::new()
+                .resource("/ping", |r| r.f(ping))
+                .boxed(),
+            App::with_state(AppState::default())
+                .prefix("/wechat")
+                .resource("/test.html", |r| r.f(index))
+                .boxed(),
+            App::with_state(AppState::default())
+                .prefix("/app1")
+                .resource("/", |r| r.f(|_r| HttpResponse::Ok()))
+                .boxed(),
+        ]
+    }).workers(1);
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
